b16beff9321f99c63b501f74836c568eec3ee837
Bradley <b.malasch@hotmail.com>
2026-04-08 12:07:03 +0200

    Refactoren code

--- LoveMatch.Trivia/Controllers/TriviaController.cs
+++ LoveMatch.Trivia/Controllers/TriviaController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace LoveMatch.Trivia.Controllers
 {
@@ -11,23 +13,26 @@ namespace LoveMatch.Trivia.Controllers
         // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
         static readonly HttpClient client = new HttpClient();
 
-        static async Task Main()
+
+        [HttpGet]
+
+        public async Task<JsonDocument> GetTriviaQuestions()
         {
             // Call asynchronous network methods in a try/catch block to handle exceptions.
             try
             {
-                using HttpResponseMessage response = await client.GetAsync("https://opentdb.com/api.php?amount=10");
+                using HttpResponseMessage response = await client.GetAsync("https://opentdb.com/api.php?amount=1");
                 response.EnsureSuccessStatusCode();
                 string responseBody = await response.Content.ReadAsStringAsync();
-                // Above three lines can be replaced with new helper method below
-                // string responseBody = await client.GetStringAsync(uri);
+                JsonDocument responseJson = JsonDocument.Parse(responseBody);
 
-                Console.WriteLine(responseBody);
+                return responseJson;
             }
             catch (HttpRequestException e)
             {
-                Console.WriteLine("\nException Caught!");
-                Console.WriteLine("Message :{0} ", e.Message);
+                string error = "Er ging iets mis";
+                JsonDocument errorJson = JsonDocument.Parse(error);
+                return errorJson;
             }
         }
     }
