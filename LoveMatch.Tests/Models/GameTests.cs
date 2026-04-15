604a0dbe1b9a7fe3107c000f8a3125ad4889df2f
Willem Smeets (2505307) <2505307smeets@zuyd.nl>
2026-04-15 10:08:27 +0200

    Added tests for Game methods

---
+++ LoveMatch.Tests/Models/GameTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LoveMatch.Models;
+
+namespace LoveMatch.Tests.Models
+{
+    public class GameTests
+    {
+        [Fact]
+        public void ChangeDifficulty_To_Easy_Returns_Easy()
+        {
+            //Arrange
+
+            Game testGameDifficulty = new Game();
+
+            //Act
+            testGameDifficulty.ChangeDifficulty("Easy");
+
+            //Assert
+            Assert.Equal("Easy", testGameDifficulty.Difficulty);
+        }
+
+        [Fact]
+
+        public void ChangePlayTime_To_90_Returns_90()
+        {
+            //Arrange
+
+            Game testGamePlayTime = new Game();
+
+            //Act
+            testGamePlayTime.ChangePlayTime(90);
+
+            //Assert
+            Assert.Equal(90, testGamePlayTime.PlayTimeInMinutes);
+        }
+    }
+}
