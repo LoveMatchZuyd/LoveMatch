f19c97b979e279008c577bbea33814422ed02eae
WSmeets-NL <willemsmeets-nl@hotmail.com>
2026-04-19 12:38:57 +0200

    Added Self-made GameFetcher API

---
+++ LoveMatch.GameFetcher.API/Data/FetchedGameContext.cs
@@ -0,0 +1,15 @@
+﻿using LoveMatch.GameFetcher.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoveMatch.GameFetcher.API.Data
+{
+    public class FetchedGameContext : DbContext
+    {
+        public FetchedGameContext(DbContextOptions<FetchedGameContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<FetchedGame> FetchedGames { get; set; }
+    }
+}
---
+++ LoveMatch.GameFetcher.API/Models/FetchedGame.cs
@@ -0,0 +1,20 @@
+﻿namespace LoveMatch.GameFetcher.API.Models;
+
+public class FetchedGame
+{
+    public int Id { get; set; } 
+
+    public string Name { get; set; }
+    public int? PlayTimeInMinutes { get; set; }
+    public string? Difficulty { get; set; }
+
+    public void ChangeDifficulty(string difficulty)
+    {
+        Difficulty = difficulty;
+    }
+
+    public void ChangePlayTime(int playtime)
+    {
+        PlayTimeInMinutes = playtime;
+    }
+}
